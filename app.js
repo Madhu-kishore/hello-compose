cd219212bca6528695321187a4be23817a2b45d8
Madhu Kishore <madhukishorekoncha@gmail.com>
2025-12-25 02:14:08 +0530

    Test webhook auto trigger

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("learning the connection of Hello from Jenkins + Docker Compose 🚀");
+  res.send("Hello from AUTO Jenkins CI/CD 🚀");
 });
 
 app.listen(3000, () => {
