36f9e184776e71618b54ea0d1d02b2b79fb3cae9
Madhu Kishore <madhukishorekoncha@gmail.com>
2025-12-25 02:18:30 +0530

    Test webhook auto trigger 2nd time

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from AUTO Jenkins CI/CD 🚀");
+  res.send("Hello from AUTO Jenkins CI/CD 🚀 ");
 });
 
 app.listen(3000, () => {
